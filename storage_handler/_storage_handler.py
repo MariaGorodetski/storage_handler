cedbe210b1658ddc772df32782041a80423b741c
Asaf Nachshon <asafnachshon@gmail.com>
2018-10-16 12:08:16 +0000

    change '_storge_handler.py' to class

--- storage_handler/_storage_handler.py
+++ storage_handler/_storage_handler.py
@@ -1,29 +1,32 @@
 from google.cloud import storage
 
 
-def upload_object(bucket_name, object_name, new_object_name):
-    client = storage.Client()
-    bucket = client.get_bucket(bucket_name)
-    if new_object_name is not None:
-        object = bucket.blob(new_object_name)
-    object = bucket.blob(object_name)
-    object.upload_from_file(object_name)
+class Storage_handler():
 
     
-def download_object(bucket_name, object_name, new_object_name):
-    client = storage.Client()
-    bucket = client.get_bucket(bucket_name)
-    object = bucket.blob(object_name)
-    if new_object_name is not None:
-        object.download_to_filename(new_object_name)
-    else:
-        object.download_to_filename(object_name)
+    def __init__(self, bucket_name):
+        client = storage.Cleint()
+        self.bucket = client.get_bucket(bucket_name)
 
 
-def objects_list(bucket_name):
-    client = storage.Client()
-    bucket = client.get_bucket(bucket_name)
-    objects = bucket.list_blobs()
+    def upload_object(bucket_name, object_name, new_object_name):
+        if new_object_name is not None:
+            object = bucket.blob(new_object_name)
+        else:
+            object = bucket.blob(object_name)
+            with open(object_name, 'rb') as file_name:
+                object.upload_from_file(file_name)
 
-    for object in objects:
-        print object.name
+    
+    def download_object(bucket_name, object_name, new_object_name):
+        object = bucket.blob(object_name)
+        if new_object_name is not None:
+            object.download_to_filename(new_object_name)
+        else:
+            object.download_to_filename(object_name)
+
+
+    def objects_list(bucket_name):
+        objects = bucket.list_blobs()
+        for object in objects:
+            print object.name
