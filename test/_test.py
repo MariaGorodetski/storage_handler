ac079693163d545ebcf99145b0e331bbca436755
Asaf Nachshon <asafnachshon@gmail.com>
2018-10-16 15:27:15 +0000

    edit test file

--- test/_test.py
+++ test/_test.py
@@ -1,12 +1,29 @@
 import unittest
 from google.cloud import storage
+import os
 from storage_handler import StorageHandler
 
-
 client = storage.Client()
-bucket = client.create_bucket("test_bucket")
+bucket = client.create_bucket('daytwo-test-bucket')
+path = "/sample_files/"
+
+object_names = []
+for f in os.listdir(path):
+    object_names += f
+object_names = sorted(object_names)
 
 
 class TestStorageHandler(unittest.TestCase):
 
-    
+    def test_upload_object():
+        for object in object_names:
+            bucket.upload_object(path+object, None)
+            bucket.upload_object(path+object, "new_"+object)
+            object_names += ["new_"+object]
+        object_names = sorted(object_names)
+        bucket_list = bucket.objects_list()
+        objects_number = len(bucket_list)
+        for i in range(objects_number):
+            assertEquals(object_names[i], bucket_list[i].name)
+            assertNotEquals(object_names[i], bucket_list[i].name.upper())
+            assertNotEquals(object_names[i], bucket_list[i].name.lower())
