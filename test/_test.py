c622f6f6b96ceb6dfbc51f65c54ba790454c49b5
Asaf Nachshon <asafnachshon@gmail.com>
2018-10-17 10:41:05 +0000

    test edit

--- test/_test.py
+++ test/_test.py
@@ -3,27 +3,41 @@ from google.cloud import storage
 import os
 from storage_handler import StorageHandler
 
-client = storage.Client()
-bucket = client.create_bucket('daytwo-test-bucket')
-path = "/sample_files/"
 
-object_names = []
-for f in os.listdir(path):
-    object_names += f
-object_names = sorted(object_names)
+bucket_name = "daytwo-test-bucket"
+client = storage.Client()
+client.create_bucket(bucket_name)
+path = './test/sample_files'
+bucket = StorageHandler(bucket_name)
 
 
 class TestStorageHandler(unittest.TestCase):
 
-    def test_upload_object():
+    def test_all(self):
+        object_names = sorted(os.listdir(path))
+        objects_in_dir = os.listdir('./')
+        new_objects_names = []
         for object in object_names:
-            bucket.upload_object(path+object, None)
-            bucket.upload_object(path+object, "new_"+object)
-            object_names += ["new_"+object]
-        object_names = sorted(object_names)
+            bucket.upload_object(path+'/'+object, None)
+            bucket.upload_object(path+'/'+object, 'new_'+object)
+            new_objects_names += ['new_'+object]
+        object_names = sorted(object_names+new_objects_names)
         bucket_list = bucket.objects_list()
-        objects_number = len(bucket_list)
+        bucket_objects = [str(o.name) for o in bucket_list]
+        objects_number = len(object_names)
+        print objects_in_dir
         for i in range(objects_number):
-            assertEquals(object_names[i], bucket_list[i].name)
-            assertNotEquals(object_names[i], bucket_list[i].name.upper())
-            assertNotEquals(object_names[i], bucket_list[i].name.lower())
+            self.assertEquals(object_names[i], bucket_objects[i])
+        new_objects_names = []
+        for object in object_names:
+            bucket.download_object(object, None)
+            bucket.download_object(object, 'down_'+object)
+            new_objects_names += ['down_'+object]
+        sample_files_objects = object_names+new_objects_names
+        object_names = sorted(sample_files_objects+objects_in_dir)
+        objects_in_dir = sorted(os.listdir('./'))
+        objects_number = len(object_names)
+        for i in range(objects_number):
+            self.assertEqual(object_names[i], objects_in_dir[i])
+        for object in sample_files_objects:
+            os.remove(object)
